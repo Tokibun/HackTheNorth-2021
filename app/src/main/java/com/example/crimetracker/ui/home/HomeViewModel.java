5905381ab5eb4d1bbdf5ad9059c5da1f9bdad10d
Abdel Rahman Mansour <abdelrahmanmansour@outlook.com>
2021-09-18 22:46:18 -0500

    Revert "DELETE THESE FLIPPING"
    This reverts commit e9b46ea4bc892750a80b21b8e6f537093512d4b4.

---
+++ app/src/main/java/com/example/crimetracker/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package com.example.crimetracker.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is map fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
